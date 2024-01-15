68ba5af0d65356f0b498d8a0c90bd9e82ac76318
Carli <carlishires@gmail.com>
2024-01-14 21:22:47 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+import Nav from "react-bootstrap/Nav"
+import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Website coming soon!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <>
+         <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+            <Container>
+               <Navbar.Brand>Carli Shires</Navbar.Brand>
+               <Navbar.Toggle aria-controls="basic-navbar-nav" />
+               <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                     <Nav.Link href="#home">Home</Nav.Link>
+                  </Nav>
+
+               </Navbar.Collapse>
+            </Container>
+         </Navbar>
+      </>
+   );
 }
 
 export default App;
